c061eeac017d4d4745716a1458fef012d444bbaf
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-06-24 21:49:38 +0800

    created code that tracks the coordinates of the block so position changes everytime you try to move it.

--- general_block_characteristic.py
+++ general_block_characteristic.py
@@ -1,4 +1,5 @@
 from color_scheme import Colors
+from block_position import BlockPosition
 import pygame
 
 class GeneralBlock:
@@ -6,9 +7,23 @@ class GeneralBlock:
         self.id=id
         self.cells={}
         self.cell_size=30
+        self.row_offset=0
+        self.column_offset=0
         self.rotation_state=0
         self.colors=Colors.block_colors()
     
+    def move(self,rows,columns):
+        self.row_offset+=rows
+        self.column_offset+=columns
+    
+    def get_position(self,screen):
+        tiles=self.cells[self.rotation_state]
+        moved_tiles=[]
+        for position in tiles:
+            position=BlockPosition(position.row+self.row_offset, position.column+self.column_offset)
+            moved_tiles.append(position)
+            return moved_tiles
+        
     def draw(self, screen):
         tiles=self.cells[self.rotation_state]
         for tile in tiles:
