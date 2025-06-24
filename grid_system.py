381b212dcd7c7d59bd342c19d4a790909ba250b1
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-06-24 18:53:45 +0800

    picked the initial colors of the blocks using hex code (will probably change later depending on how good it complements the background)

--- grid_system.py
+++ grid_system.py
@@ -4,9 +4,21 @@ class GridSystem:
         self.num_cols=10
         self.cell_size=30
         self.grid=[[0 for j in range(self.num_cols)] for i in range(self.num_rows)]
+        self.colors= self.block_colors()
 
     def print_grid(self):
         for row in range(self.num_rows):
             for column in range(self.num_cols):
                 print(self.grid[row][column], end=" ")
             print()
+    
+    def block_colors(self):
+        red = (153,15,2)
+        orange=(216,124,33)
+        yellow = (255,246,0)
+        green = (0,106,78)
+        blue = (22,30,153)
+        indigo=(0,157,175)
+        violet = (53,28,117)
+
+        return [red,orange,yellow,green,blue,indigo,violet]
