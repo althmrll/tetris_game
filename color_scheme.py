62850f6023e068722dadc16380a43652c7013ec4
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-06-24 19:33:28 +0800

    copied and pasted the code from grid system to this class, I thought it's so you can just call it from the different files, I guess not?

---
+++ color_scheme.py
@@ -0,0 +1,13 @@
+class Colors:
+    empty=(16,17,30)
+    red = (153,15,2)
+    orange=(216,124,33)
+    yellow = (255,246,0)
+    green = (0,106,78)
+    blue = (22,30,153)
+    indigo=(0,157,175)
+    violet = (53,28,117)
+
+    @classmethod
+    def block_colors(cls):
+        return [cls.empty, cls.red, cls.orange, cls.yellow, cls.green, cls.blue, cls.indigo,cls.violet]
