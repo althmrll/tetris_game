cf1ff07f2e00335cf3cfadcaaae924fb97a670c9
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-06-24 19:42:54 +0800

    created a position file so it will be easier ot track the blocks position. Also added a draw definition in the block characteristic so it will show on screen (although no content yet)

--- general_block_characteristic.py
+++ general_block_characteristic.py
@@ -1,7 +1,12 @@
+from color_scheme import Colors
+
 class GeneralBlock:
     def __init__(self,id):
         self.id=id
         self.cells={}
         self.cell_size=30
         self.rotation_state=0
-        
+        self.colors=Colors.block_colors()
+    
+    def draw(self, screen):
+        ...
