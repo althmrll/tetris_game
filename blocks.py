0b204c6e12c186ed3917740f8616b1638a7d0787
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-06-24 21:18:48 +0800

    finished the O-block by changing the positions only. (position was given in the video)

--- blocks.py
+++ blocks.py
@@ -1,14 +1,14 @@
 from general_block_characteristic import GeneralBlock
 from block_position import BlockPosition
 
-class LBlock(GeneralBlock):
+class OBlock(GeneralBlock):
     def __init__(self):
         super().__init__(id=1)
         self.cells={
-            0: [BlockPosition(0,2),BlockPosition(1,0),BlockPosition(1,1), BlockPosition(1,2)],
-            1: [BlockPosition(0,1),BlockPosition(1,1),BlockPosition(2,1), BlockPosition(2,2)],
-            2: [BlockPosition(1,0),BlockPosition(1,1),BlockPosition(1,2), BlockPosition(2,0)],
-            3: [BlockPosition(0,0),BlockPosition(0,1),BlockPosition(1,1), BlockPosition(2,1)]
+            0: [BlockPosition(0,0),BlockPosition(0,1),BlockPosition(1,0), BlockPosition(1,1)],
+            1: [BlockPosition(0,0),BlockPosition(0,1),BlockPosition(1,0), BlockPosition(1,1)],
+            2: [BlockPosition(0,0),BlockPosition(0,1),BlockPosition(1,0), BlockPosition(1,1)],
+            3: [BlockPosition(0,0),BlockPosition(0,1),BlockPosition(1,0), BlockPosition(1,1)]
         }
 
 class LBlock(GeneralBlock):
