d9e522e7f77979bab881074cacc54ae4d938bb7f
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-06-24 19:37:35 +0800

    I was right, I deleted the colors in the frid system and just called the block colors from the colors class in the game interface.

--- grid_system.py
+++ grid_system.py
@@ -1,4 +1,5 @@
 import pygame
+from color_scheme import Colors
 
 class GridSystem:
     def __init__(self):
@@ -6,25 +7,13 @@ class GridSystem:
         self.num_cols=10
         self.cell_size=30
         self.grid=[[0 for j in range(self.num_cols)] for i in range(self.num_rows)]
-        self.colors= self.block_colors()
+        self.colors= Colors.block_colors()
 
     def print_grid(self):
         for row in range(self.num_rows):
             for column in range(self.num_cols):
                 print(self.grid[row][column], end=" ")
             print()
-    
-    def block_colors(self):
-        empty=(16,17,30)
-        red = (153,15,2)
-        orange=(216,124,33)
-        yellow = (255,246,0)
-        green = (0,106,78)
-        blue = (22,30,153)
-        indigo=(0,157,175)
-        violet = (53,28,117)
-
-        return [empty, red,orange,yellow,green,blue,indigo,violet]
     
     def draw(self,screen):
         for row in range(self.num_rows):
