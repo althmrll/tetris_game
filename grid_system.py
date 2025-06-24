af9c8951518c5b1d9f09e74855561bbb5bcb33d4
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-06-24 19:06:08 +0800

    modified the draw definition so grid lines will show on the screen.

--- grid_system.py
+++ grid_system.py
@@ -29,5 +29,5 @@ class GridSystem:
         for row in range(self.num_rows):
             for column in range (self.num_cols):
                 cell_value = self.grid[row][column]
-                cell_rect=pygame.Rect(column*self.cell_size, row*self.cell_size,self.cell_size,self.cell_size)
+                cell_rect=pygame.Rect(column*self.cell_size+1, row*self.cell_size+1,self.cell_size-1,self.cell_size-1)
                 pygame.draw.rect(screen, self.colors[cell_value], cell_rect)
