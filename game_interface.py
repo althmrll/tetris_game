cf3314b6b9ee2b7e94c7e3fbc1134b2b772a3c3c
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-06-24 17:59:05 +0800

    used the time attribute of pygame to determine the frame rate of the game

--- game_interface.py
+++ game_interface.py
@@ -2,4 +2,7 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode((300,600))
+screen = pygame.display.set_mode((300,600)) #main_window
+pygame.display.set_caption("Tetris Game") #title of the window
+
+clock=pygame.time.Clock() #Determines frame rate of game
