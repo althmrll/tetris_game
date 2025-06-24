02b8b051457a18705a10aa922f7c8d7ef968cd78
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-06-24 21:20:57 +0800

    moved the Oblock to the 6th id since I think it will look better tealy than red. And instead made the I-block first.(id is the index number of the colors in the color scheme, it will determine what color the block will be)

--- blocks.py
+++ blocks.py
@@ -1,24 +1,14 @@
 from general_block_characteristic import GeneralBlock
 from block_position import BlockPosition
 
-class OBlock(GeneralBlock):
+class IBlock(GeneralBlock):
     def __init__(self):
         super().__init__(id=1)
         self.cells={
-            0: [BlockPosition(0,0),BlockPosition(0,1),BlockPosition(1,0), BlockPosition(1,1)],
-            1: [BlockPosition(0,0),BlockPosition(0,1),BlockPosition(1,0), BlockPosition(1,1)],
-            2: [BlockPosition(0,0),BlockPosition(0,1),BlockPosition(1,0), BlockPosition(1,1)],
-            3: [BlockPosition(0,0),BlockPosition(0,1),BlockPosition(1,0), BlockPosition(1,1)]
-        }
-
-class LBlock(GeneralBlock):
-    def __init__(self):
-        super().__init__(id=2)
-        self.cells={
-            0: [BlockPosition(0,2),BlockPosition(1,0),BlockPosition(1,1), BlockPosition(1,2)],
-            1: [BlockPosition(0,1),BlockPosition(1,1),BlockPosition(2,1), BlockPosition(2,2)],
-            2: [BlockPosition(1,0),BlockPosition(1,1),BlockPosition(1,2), BlockPosition(2,0)],
-            3: [BlockPosition(0,0),BlockPosition(0,1),BlockPosition(1,1), BlockPosition(2,1)]
+            0: [BlockPosition(1,0),BlockPosition(1,1),BlockPosition(1,2), BlockPosition(1,3)],
+            1: [BlockPosition(0,2),BlockPosition(1,2),BlockPosition(2,2), BlockPosition(3,2)],
+            2: [BlockPosition(2,0),BlockPosition(2,1),BlockPosition(2,2), BlockPosition(2,3)],
+            3: [BlockPosition(0,1),BlockPosition(1,1),BlockPosition(2,1), BlockPosition(3,1)]
         }
 
 class LBlock(GeneralBlock):
@@ -51,14 +41,14 @@ class LBlock(GeneralBlock):
             3: [BlockPosition(0,0),BlockPosition(0,1),BlockPosition(1,1), BlockPosition(2,1)]
         }
 
-class LBlock(GeneralBlock):
+class OBlock(GeneralBlock):
     def __init__(self):
         super().__init__(id=6)
         self.cells={
-            0: [BlockPosition(0,2),BlockPosition(1,0),BlockPosition(1,1), BlockPosition(1,2)],
-            1: [BlockPosition(0,1),BlockPosition(1,1),BlockPosition(2,1), BlockPosition(2,2)],
-            2: [BlockPosition(1,0),BlockPosition(1,1),BlockPosition(1,2), BlockPosition(2,0)],
-            3: [BlockPosition(0,0),BlockPosition(0,1),BlockPosition(1,1), BlockPosition(2,1)]
+            0: [BlockPosition(0,0),BlockPosition(0,1),BlockPosition(1,0), BlockPosition(1,1)],
+            1: [BlockPosition(0,0),BlockPosition(0,1),BlockPosition(1,0), BlockPosition(1,1)],
+            2: [BlockPosition(0,0),BlockPosition(0,1),BlockPosition(1,0), BlockPosition(1,1)],
+            3: [BlockPosition(0,0),BlockPosition(0,1),BlockPosition(1,0), BlockPosition(1,1)]
         }
 
 class LBlock(GeneralBlock):
