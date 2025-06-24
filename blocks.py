cc477c4c9315280c1a4d6c3ffd4d7d969ee31904
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-06-24 21:35:42 +0800

    finished the zblock by changing block position(block position in given in video). Put it in the 4th id leaving the L block to be blue

--- blocks.py
+++ blocks.py
@@ -31,14 +31,14 @@ class JBlock(GeneralBlock):
             3: [BlockPosition(0,1),BlockPosition(1,1),BlockPosition(2,0), BlockPosition(2,1)]
         }
 
-class LBlock(GeneralBlock):
+class ZBlock(GeneralBlock):
     def __init__(self):
         super().__init__(id=4)
         self.cells={
-            0: [BlockPosition(0,2),BlockPosition(1,0),BlockPosition(1,1), BlockPosition(1,2)],
-            1: [BlockPosition(0,1),BlockPosition(1,1),BlockPosition(2,1), BlockPosition(2,2)],
-            2: [BlockPosition(1,0),BlockPosition(1,1),BlockPosition(1,2), BlockPosition(2,0)],
-            3: [BlockPosition(0,0),BlockPosition(0,1),BlockPosition(1,1), BlockPosition(2,1)]
+            0: [BlockPosition(0,0),BlockPosition(0,1),BlockPosition(1,1), BlockPosition(1,2)],
+            1: [BlockPosition(0,2),BlockPosition(1,1),BlockPosition(1,2), BlockPosition(2,1)],
+            2: [BlockPosition(1,0),BlockPosition(1,1),BlockPosition(2,1), BlockPosition(2,2)],
+            3: [BlockPosition(0,1),BlockPosition(1,0),BlockPosition(1,1), BlockPosition(2,0)]
         }
 
 class LBlock(GeneralBlock):
