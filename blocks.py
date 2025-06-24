f0bef947ac68b4787a6ff453c028abe8401dd258
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-06-24 21:17:06 +0800

    turns out, you need to create more classes for each block. For now, I will create each class in the same file, but will separate each later. I duplicated the code for the Lblock 6 times for the other type blocks, changing their id which corresponds to the colors I chose based on their index on the list.

--- blocks.py
+++ blocks.py
@@ -10,3 +10,63 @@ class LBlock(GeneralBlock):
             2: [BlockPosition(1,0),BlockPosition(1,1),BlockPosition(1,2), BlockPosition(2,0)],
             3: [BlockPosition(0,0),BlockPosition(0,1),BlockPosition(1,1), BlockPosition(2,1)]
         }
+
+class LBlock(GeneralBlock):
+    def __init__(self):
+        super().__init__(id=2)
+        self.cells={
+            0: [BlockPosition(0,2),BlockPosition(1,0),BlockPosition(1,1), BlockPosition(1,2)],
+            1: [BlockPosition(0,1),BlockPosition(1,1),BlockPosition(2,1), BlockPosition(2,2)],
+            2: [BlockPosition(1,0),BlockPosition(1,1),BlockPosition(1,2), BlockPosition(2,0)],
+            3: [BlockPosition(0,0),BlockPosition(0,1),BlockPosition(1,1), BlockPosition(2,1)]
+        }
+
+class LBlock(GeneralBlock):
+    def __init__(self):
+        super().__init__(id=3)
+        self.cells={
+            0: [BlockPosition(0,2),BlockPosition(1,0),BlockPosition(1,1), BlockPosition(1,2)],
+            1: [BlockPosition(0,1),BlockPosition(1,1),BlockPosition(2,1), BlockPosition(2,2)],
+            2: [BlockPosition(1,0),BlockPosition(1,1),BlockPosition(1,2), BlockPosition(2,0)],
+            3: [BlockPosition(0,0),BlockPosition(0,1),BlockPosition(1,1), BlockPosition(2,1)]
+        }
+
+class LBlock(GeneralBlock):
+    def __init__(self):
+        super().__init__(id=4)
+        self.cells={
+            0: [BlockPosition(0,2),BlockPosition(1,0),BlockPosition(1,1), BlockPosition(1,2)],
+            1: [BlockPosition(0,1),BlockPosition(1,1),BlockPosition(2,1), BlockPosition(2,2)],
+            2: [BlockPosition(1,0),BlockPosition(1,1),BlockPosition(1,2), BlockPosition(2,0)],
+            3: [BlockPosition(0,0),BlockPosition(0,1),BlockPosition(1,1), BlockPosition(2,1)]
+        }
+
+class LBlock(GeneralBlock):
+    def __init__(self):
+        super().__init__(id=5)
+        self.cells={
+            0: [BlockPosition(0,2),BlockPosition(1,0),BlockPosition(1,1), BlockPosition(1,2)],
+            1: [BlockPosition(0,1),BlockPosition(1,1),BlockPosition(2,1), BlockPosition(2,2)],
+            2: [BlockPosition(1,0),BlockPosition(1,1),BlockPosition(1,2), BlockPosition(2,0)],
+            3: [BlockPosition(0,0),BlockPosition(0,1),BlockPosition(1,1), BlockPosition(2,1)]
+        }
+
+class LBlock(GeneralBlock):
+    def __init__(self):
+        super().__init__(id=6)
+        self.cells={
+            0: [BlockPosition(0,2),BlockPosition(1,0),BlockPosition(1,1), BlockPosition(1,2)],
+            1: [BlockPosition(0,1),BlockPosition(1,1),BlockPosition(2,1), BlockPosition(2,2)],
+            2: [BlockPosition(1,0),BlockPosition(1,1),BlockPosition(1,2), BlockPosition(2,0)],
+            3: [BlockPosition(0,0),BlockPosition(0,1),BlockPosition(1,1), BlockPosition(2,1)]
+        }
+
+class LBlock(GeneralBlock):
+    def __init__(self):
+        super().__init__(id=7)
+        self.cells={
+            0: [BlockPosition(0,2),BlockPosition(1,0),BlockPosition(1,1), BlockPosition(1,2)],
+            1: [BlockPosition(0,1),BlockPosition(1,1),BlockPosition(2,1), BlockPosition(2,2)],
+            2: [BlockPosition(1,0),BlockPosition(1,1),BlockPosition(1,2), BlockPosition(2,0)],
+            3: [BlockPosition(0,0),BlockPosition(0,1),BlockPosition(1,1), BlockPosition(2,1)]
+        }
