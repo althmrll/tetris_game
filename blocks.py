bb5dcb6abfe71645fa2ac02636df8ae39488e841
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-06-24 21:27:07 +0800

    inputted the block position for j-block (position was given in video), realized I accidentally deleted one class so I duplicated the l-block again and created the J-block in third id since I think it will look good yellow

--- blocks.py
+++ blocks.py
@@ -13,12 +13,22 @@ class IBlock(GeneralBlock):
 
 class LBlock(GeneralBlock):
     def __init__(self):
-        super().__init__(id=3)
+        super().__init__(id=2)
         self.cells={
             0: [BlockPosition(0,2),BlockPosition(1,0),BlockPosition(1,1), BlockPosition(1,2)],
             1: [BlockPosition(0,1),BlockPosition(1,1),BlockPosition(2,1), BlockPosition(2,2)],
             2: [BlockPosition(1,0),BlockPosition(1,1),BlockPosition(1,2), BlockPosition(2,0)],
             3: [BlockPosition(0,0),BlockPosition(0,1),BlockPosition(1,1), BlockPosition(2,1)]
+        }
+
+class JBlock(GeneralBlock):
+    def __init__(self):
+        super().__init__(id=3)
+        self.cells={
+            0: [BlockPosition(0,0),BlockPosition(1,0),BlockPosition(1,1), BlockPosition(1,2)],
+            1: [BlockPosition(0,1),BlockPosition(0,2),BlockPosition(1,1), BlockPosition(2,1)],
+            2: [BlockPosition(1,0),BlockPosition(1,1),BlockPosition(1,2), BlockPosition(2,2)],
+            3: [BlockPosition(0,1),BlockPosition(1,1),BlockPosition(2,0), BlockPosition(2,1)]
         }
 
 class LBlock(GeneralBlock):
