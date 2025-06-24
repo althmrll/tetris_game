8661bbf35f306988b804cc1aca37f95256f643a2
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-06-24 18:05:07 +0800

    this project was based off of a youtube video, since it is hard to separate it, I will be doing the coding in the game interface file first before separating it into different classes/ files. Change of topic, I'm setting up the game loop so the game will run

--- game_interface.py
+++ game_interface.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame,sys
 
 pygame.init()
 
@@ -6,3 +6,10 @@ screen = pygame.display.set_mode((300,600)) #main_window
 pygame.display.set_caption("Tetris Game") #title of the window
 
 clock=pygame.time.Clock() #Determines frame rate of game
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit
+        
