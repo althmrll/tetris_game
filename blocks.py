c042d875402bfe281f2660af1955551f05d166b9
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-06-24 21:31:06 +0800

    changed position for the s-block making it color violet(position is given in video)

--- blocks.py
+++ blocks.py
@@ -61,12 +61,12 @@ class OBlock(GeneralBlock):
             3: [BlockPosition(0,0),BlockPosition(0,1),BlockPosition(1,0), BlockPosition(1,1)]
         }
 
-class LBlock(GeneralBlock):
+class SBlock(GeneralBlock):
     def __init__(self):
         super().__init__(id=7)
         self.cells={
-            0: [BlockPosition(0,2),BlockPosition(1,0),BlockPosition(1,1), BlockPosition(1,2)],
-            1: [BlockPosition(0,1),BlockPosition(1,1),BlockPosition(2,1), BlockPosition(2,2)],
-            2: [BlockPosition(1,0),BlockPosition(1,1),BlockPosition(1,2), BlockPosition(2,0)],
-            3: [BlockPosition(0,0),BlockPosition(0,1),BlockPosition(1,1), BlockPosition(2,1)]
+            0: [BlockPosition(0,1),BlockPosition(0,2),BlockPosition(1,0), BlockPosition(1,1)],
+            1: [BlockPosition(0,1),BlockPosition(1,1),BlockPosition(1,2), BlockPosition(2,2)],
+            2: [BlockPosition(1,1),BlockPosition(1,2),BlockPosition(2,0), BlockPosition(2,1)],
+            3: [BlockPosition(0,0),BlockPosition(1,0),BlockPosition(1,1), BlockPosition(2,1)]
         }
