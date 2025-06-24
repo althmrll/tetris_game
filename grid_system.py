a599a6ebd8cdc27bfa538bab04b4a26e1ba67bae
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-06-24 18:35:51 +0800

    created the grid by using for loop again

--- grid_system.py
+++ grid_system.py
@@ -4,3 +4,9 @@ class GridSystem:
         self.num_cols=10
         self.cell_size=30
         self.grid=[[0 for j in range(self.num_cols)] for i in range(self.num_rows)]
+
+    def print_grid(self):
+        for row in range(self.num_rows):
+            for column in range(self.num_cols):
+                print(self.grid[row][column], end=" ")
+            print()
