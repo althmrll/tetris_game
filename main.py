0f09214ab605f80754f3ac6ce88de140ed985016
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-06-26 22:22:31 +0800

    called the newly made definitions in the main code and time to test!

--- main.py
+++ main.py
@@ -19,6 +19,13 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit
+        if event.type==pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                game.move_left()
+            if event.key == pygame.K_RIGHT:
+                game.move_right()
+            if event.key == pygame.K_DOWN:
+                game.move_down()
 
     pygame.display.update()
     clock.tick(60)
