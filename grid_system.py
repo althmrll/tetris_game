477cfd6d3e2e363f20f4d4d4fddaf998021bd32e
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-06-26 22:29:50 +0800

    blocks now move, woohoo! but the blocks continue moving even if they are out of the screen, so we decided to put a limit they stop when they reach th eend of the screen

--- grid_system.py
+++ grid_system.py
@@ -15,6 +15,10 @@ class GridSystem:
                 print(self.grid[row][column], end=" ")
             print()
     
+    def block_position_limit(self, row, column):
+        if row>=0 and row<self.num_rows and column>=0 and column<self.num_cols:
+            return True
+        return False
     def draw(self,screen):
         for row in range(self.num_rows):
             for column in range (self.num_cols):
