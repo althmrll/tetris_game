33b8aae6fd88cc1a3d69e60494d1da224fa4e4ce
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-06-26 21:48:12 +0800

    for the game mechanics, I first assigned values to the vriables I will need for the game_mechanics (As stated by the video)

--- game_mechanics.py
+++ game_mechanics.py
@@ -3,3 +3,11 @@ from blocks import *
 import random
 
 class Game:
+    def __init__(self):
+        self.grid = GridSystem()
+        self.blocks = [IBlock(),JBlock(),LBlock(),OBlock(),SBlock(),TBlock(),ZBlock()]
+        self.current_block = self.random_block()
+        self.next_block = self.random_block()
+
+    def random_block(self):
+        ...
