b83852a2b060ca900c020b778901f452cc321b67
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-06-26 21:55:48 +0800

    created the code to randomize the blocks that wil be flashed on screen. Changed the order of the blocks in the self.blocks according to the id I assgned to each earlier

--- game_mechanics.py
+++ game_mechanics.py
@@ -5,9 +5,14 @@ import random
 class Game:
     def __init__(self):
         self.grid = GridSystem()
-        self.blocks = [IBlock(),JBlock(),LBlock(),OBlock(),SBlock(),TBlock(),ZBlock()]
+        self.blocks = [IBlock(),TBlock(),JBlock(),ZBlock(),LBlock(),OBlock(),SBlock()]
         self.current_block = self.random_block()
         self.next_block = self.random_block()
 
     def random_block(self):
-        ...
+        if len(self.blocks)==0:
+            self.blocks = [IBlock(),TBlock(),JBlock(),ZBlock(),LBlock(),OBlock(),SBlock()]
+        block=random.choices(self.blocks)
+        self.blocks.remove(block)
+        return block
+        
