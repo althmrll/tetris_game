461f390b6a4c903ee4b460b5aa93f6a0c80152a5
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-06-26 21:41:48 +0800

    i cut the ocntents of the game_mechanics and put it in the main file, because I think it should be part of the main.

--- main.py
+++ main.py
@@ -1,2 +1,30 @@
-if __name__=="__main__":
-    ...
+import pygame,sys
+from grid_system import GridSystem
+from blocks import *
+
+#variables
+line_color=(45,59,67)
+
+pygame.init()
+
+screen = pygame.display.set_mode((300,600)) #main_window
+pygame.display.set_caption("Tetris Game") #title of the window
+
+clock=pygame.time.Clock() #Determines frame rate of game
+game_grid=GridSystem()
+
+block=OBlock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit
+
+    pygame.display.update()
+    clock.tick(60)
+
+#design
+    screen.fill(line_color)
+    game_grid.draw(screen)
+    block.draw(screen)
