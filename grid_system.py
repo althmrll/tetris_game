8ee1f1888ee246d834b94d5ad1660c4be494077e
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-06-27 14:42:12 +0800

    wrote code so that full rows are cleared and the remaning rows move down

--- grid_system.py
+++ grid_system.py
@@ -34,8 +34,27 @@ class GridSystem:
         for column in range(self.num_cols):
             if self.grid[row][column]==0:
                 return False
-            return True
+        return True
         
+    def clear_cell_value(self,row):
+        for column in range(self.num_cols):
+            self.grid[row][column]=0
+    
+    def move_row_down(self,row,num_rows):
+        for column in range(self.num_cols):
+            self.grid[row-num_rows][column]=self.grid[row][column]
+            self.grid[row][column]=0
+        
+    def clear_full_row(self):
+        completed=0
+        for row in range(self.num_rows-1,0,-1):
+            if self.check_if_row_is_full(row):
+                self.clear_row(row)
+                completed+=1
+            elif completed>0:
+                self.move_row_down(row, completed)
+        return completed
+
     def draw(self,screen):       
         for row in range(self.num_rows):
             for column in range (self.num_cols):
