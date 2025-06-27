0543a460bad8d35be64008c19e06f995be368db6
althmrll <altheamariellcdelara@iskolarngbayan.pup.edu.ph>
2025-06-27 18:23:17 +0800

    working now, but  I cannot see the score, I watched it and i think it's because I messed up the order, hopiing this works.

--- main.py
+++ main.py
@@ -47,11 +47,11 @@ while True:
 
     screen.fill(Colors.line_color)
     screen.blit(score,(365,20,50,50))
-    screen.blit(score_value,score_value.get_rect(centerx=score_rect.centerx,centery=score_rect.centery))
     screen.blit(next_block,(370,180,50,50))
     if game.game_over==True:
         screen.blit(game_over_message,(330,450,50,50))
     pygame.draw.rect(screen,Colors.empty,score_rect,0,10)
+    screen.blit(score_value,score_value.get_rect(centerx=score_rect.centerx,centery=score_rect.centery))
     pygame.draw.rect(screen,Colors.empty,next_block_rect,0,10)
     game.draw(screen)
     
